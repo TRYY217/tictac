a9c9a8361402d7e37b85a058c8e09279eb942ca0
tictactoeproject <vizenkovandrej@gmail.com>
2025-06-01 20:58:09 +0300

    site commit №1

--- logic/game_field.py
+++
@@ -1,9 +0,0 @@
-def draw_grid(board):
-    for i in range(3):
-        row = " | ".join(board[i])
-        print(row)
-        if i < 2:
-            print("--+---+--")
-
-# Пустое поле 3x3
-board = [[" " for _ in range(3)] for _ in range(3)]
